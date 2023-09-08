528cf371fe2b22d8a57ea576dace7053ba69fd0b
Adam Clint Muana <Adam.Muana@entaingroup.com>
2023-09-08 14:48:14 +0800

    dynamic prizes added

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ modalBtn.forEach((btn) => {
 closeBtn.addEventListener('click', () => {
     modal.classList.remove('open-modal');
 })
+
+
+const imgBtns = document.querySelectorAll('.img-click')
+const rewardContainers = document.querySelectorAll('.reward-display-img')
+
+const claimBtn = document.querySelectorAll('.claim-cta')
+claimBtn.forEach((btn) => {
+    btn.classList.add('remove-pointer-event')
+})
+
+const rewardTitle = document.getElementById('header-title')
+const rewardDescription = document.querySelector('.header-bottom p')
+const rewardPoints = document.querySelector('.points')
+
+const modalImg = document.querySelector('.modal-container img')
+const modalTitle = document.querySelector('.modal-bottom-title')
+const modalPoints = document.querySelector('.modal-points')
+
+imgBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        rewardContainers.forEach((container) => {
+            container.setAttribute('src', e.target.getAttribute('src'))
+        })
+
+        claimBtn.forEach((btn) => {
+            btn.classList.remove('remove-pointer-event')
+        })
+
+        modalImg.setAttribute('src', e.target.getAttribute('src'))
+
+        rewardTitle.innerHTML = e.target.parentElement.getElementsByClassName('reward-title')[0].innerHTML
+        rewardDescription.innerHTML = e.target.parentElement.getElementsByClassName('reward-description')[0].innerHTML
+        rewardPoints.innerHTML = e.target.parentElement.getElementsByClassName('reward-points')[0].innerHTML
+
+        modalTitle.innerHTML = e.target.parentElement.getElementsByClassName('reward-title')[0].innerHTML
+        modalPoints.innerHTML = e.target.parentElement.getElementsByClassName('reward-points')[0].innerHTML
+    })
+})
